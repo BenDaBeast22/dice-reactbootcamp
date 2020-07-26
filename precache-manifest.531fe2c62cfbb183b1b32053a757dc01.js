54a50bbc5c683e407d46c17b101e61a0c113931f
ben critoph <bencritoph@gmail.com>
2020-07-26 18:33:29 -0400

    Updates

---
+++ precache-manifest.531fe2c62cfbb183b1b32053a757dc01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae7f9ab60b40ac941d3bc382897c31f",
+    "url": "/dice-reactbootcamp/index.html"
+  },
+  {
+    "revision": "1cb17551103d20fb413e",
+    "url": "/dice-reactbootcamp/static/css/main.782ed6d9.chunk.css"
+  },
+  {
+    "revision": "14cc1f35bded0f88e7dd",
+    "url": "/dice-reactbootcamp/static/js/2.1212b618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-reactbootcamp/static/js/2.1212b618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb17551103d20fb413e",
+    "url": "/dice-reactbootcamp/static/js/main.744c894b.chunk.js"
+  },
+  {
+    "revision": "32efd24997eea6dc91b7",
+    "url": "/dice-reactbootcamp/static/js/runtime-main.00ae6556.js"
+  }
+]);
